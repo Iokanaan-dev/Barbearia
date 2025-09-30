a20206fd763052f07d457093bc7799ae7c4fd806
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2025-09-30 10:49:57 -0300

    Individuo

--- src/main/java/com/mycompany/barbearia/individuo/Individuo.java
+++ src/main/java/com/mycompany/barbearia/individuo/Individuo.java
@@ -9,5 +9,58 @@ package com.mycompany.barbearia.individuo;
  * @author italo
  */
 public class Individuo {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String data_nascimento;
+    private int id;
+    
+    public Individuo(String nome, String cpf, String telefone, String data_nascimento){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     
 }
